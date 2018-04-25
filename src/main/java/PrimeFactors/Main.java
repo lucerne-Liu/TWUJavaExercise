63bdc7adeb39591351ad1a6f96d96734fd1f92d5
Lucerne <lucerne_liu@foxmail.com>
2018-04-25 18:40:58 +0800

    feat:finish "Prime Factors Exercise"

---
+++ src/main/java/PrimeFactors/Main.java
@@ -0,0 +1,9 @@
+package PrimeFactors;
+
+public class Main {
+    public static void main(String[] args) {
+        Prime prime = new Prime(30);
+        prime.findPrime();
+        prime.print();
+    }
+}
---
+++ src/main/java/PrimeFactors/Prime.java
@@ -0,0 +1,27 @@
+package PrimeFactors;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Prime {
+    private List<Integer> list = new ArrayList();
+    private int num;
+
+    public Prime(int num) {
+        this.num = num;
+    }
+
+    public void findPrime() {
+        for (int i = 2; i <= num; i++) {
+            if (num % i == 0) {
+                list.add(i);
+                num = num / i;
+                i--;
+            }
+        }
+    }
+
+    public void print() {
+        System.out.println(list.stream().sorted().map(String::valueOf).reduce((a,b) -> a +"," +b).get());
+    }
+}
